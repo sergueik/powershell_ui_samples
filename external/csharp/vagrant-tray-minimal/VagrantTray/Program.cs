3480cda09ea2ffb6b52c008fe789673b2de93c8b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-02-15 19:23:41 -0500

    intermediate revision - clening up the project for basics

---
+++ external/csharp/vagrant-tray-minimal/VagrantTray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HudsonClient {
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (ProcessIcon pi = new ProcessIcon())
+            {
+                pi.Display();
+
+                Application.Run();
+            }
+        }
+    }
+
+}
