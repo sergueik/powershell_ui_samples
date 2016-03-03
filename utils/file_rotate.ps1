cbf929a1d27212c5b6f6c9b06345391573c437cf
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-03-03 18:43:33 -0500

    intermediate revision  - make script Powershell 2.0 compatible

--- utils/file_rotate.ps1
+++ utils/file_rotate.ps1
@@ -1,6 +1,6 @@
 # setting the mockup environment
 
-$statedir = 'C:\Users\u446462\AppData\Local\Temp'
+$statedir = $env:TEMP
 
 $last_run_report = 'last_run_report.yaml'
 $filename_mask = ('{0}.*' -f $last_run_report)
@@ -20,7 +20,7 @@ if (test-path -path $statedir) {
   
   if (test-path -path $last_run_report) {
       
-    $file_count = ( Get-ChildItem -Name "$last_run_report.*" -ErrorAction 'Stop' ).count
+    $file_count = @( Get-ChildItem -Name "$last_run_report.*" -ErrorAction 'Stop' ).count
     
     if ($file_count -gt 0) {
       $file_count..1  | foreach-object { 
