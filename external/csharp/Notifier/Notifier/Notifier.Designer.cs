c5c78c28074ce57810e3226a5d298ec35dc486f3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-08-26 12:00:20 -0400

    initial snapshot

---
+++ external/csharp/Notifier/Notifier/Notifier.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Notify
+{
+    partial class Notifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDesc = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.icon = new System.Windows.Forms.PictureBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.icon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelDesc
+            // 
+            this.labelDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDesc.Image = global::Notify.Properties.Resources.notifier;
+            this.labelDesc.Location = new System.Drawing.Point(42, 30);
+            this.labelDesc.Name = "labelDesc";
+            this.labelDesc.Size = new System.Drawing.Size(270, 56);
+            this.labelDesc.TabIndex = 3;
+            this.labelDesc.Text = "Description";
+            this.labelDesc.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.Image = global::Notify.Properties.Resources.notifier;
+            this.date.Location = new System.Drawing.Point(43, 86);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(19, 9);
+            this.date.TabIndex = 4;
+            this.date.Text = "date";
+            // 
+            // icon
+            // 
+            this.icon.BackgroundImage = global::Notify.Properties.Resources.img_notifier;
+            this.icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.icon.Image = global::Notify.Properties.Resources.info;
+            this.icon.Location = new System.Drawing.Point(12, 46);
+            this.icon.Name = "icon";
+            this.icon.Size = new System.Drawing.Size(24, 24);
+            this.icon.TabIndex = 2;
+            this.icon.TabStop = false;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(140)))), ((int)(((byte)(230)))));
+            this.buttonClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(115)))), ((int)(((byte)(135)))));
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.ForeColor = System.Drawing.Color.White;
+            this.buttonClose.Image = global::Notify.Properties.Resources.close;
+            this.buttonClose.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonClose.Location = new System.Drawing.Point(2, 2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(320, 24);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = " Calibrator";
+            this.buttonClose.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Notifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Notify.Properties.Resources.notifier;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(324, 100);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.labelDesc);
+            this.Controls.Add(this.icon);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Notifier";
+            this.ShowInTaskbar = false;
+            this.Text = "Notifier";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.OnLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.icon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.PictureBox icon;
+        private System.Windows.Forms.Label labelDesc;
+        private System.Windows.Forms.Label date;
+    }
+}
---
+++ external/csharp/Notifier/Notifier/Notifier.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Notify
+{
+    public partial class Notifier : Form
+    {
+        #region GLOBALS
+        // Set the type of the notify
+        public enum Type { INFO, WARNING, ERROR, OK }
+        
+        // Helper class for handle notifier position
+        class PosXY{
+            internal int X;
+            internal int Y;
+
+            public PosXY(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+        static List<PosXY> posXY = new List<PosXY>();
+
+        private string desc;
+        private PosXY myPos;
+        Type type = Type.INFO;
+        String title = "Notifier";
+        #endregion
+
+        #region CONSTRUCTOR & CREATE
+        public Notifier(string dsc, Type type = Type.INFO, string tit = "Notifier")
+        {
+            this.desc = dsc;
+            this.type = type;
+            this.title = tit;
+
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            BackColor = Color.Gray;
+            TransparencyKey = Color.Gray;
+            FormBorderStyle = FormBorderStyle.None;
+
+            this.Tag = "__Notifier";
+
+            // Fill the note
+            buttonClose.Text = title;
+            labelDesc.Text = desc;
+            date.Text = DateTime.Now + "";
+
+            switch (type)
+            {
+                case Type.ERROR:
+                    icon.Image = global::Notify.Properties.Resources.ko;
+                    buttonClose.BackColor = Color.FromArgb(200, 60, 70);
+                    break;
+                case Type.INFO: 
+                    icon.Image = global::Notify.Properties.Resources.info;
+                    buttonClose.BackColor = Color.FromArgb(90, 140, 230);
+                    break;
+                case Type.WARNING:
+                    icon.Image = global::Notify.Properties.Resources.warning;
+                    buttonClose.BackColor = Color.FromArgb(200, 200, 80);
+                    break;
+                case Type.OK:
+                    icon.Image = global::Notify.Properties.Resources.ok;
+                    buttonClose.BackColor = Color.FromArgb(80, 200, 130);
+                    break;
+            }
+            Rectangle rec = Screen.GetWorkingArea(this);
+
+            // Add position
+            // Check for a available Position
+            int maxNot = rec.Height / this.Height;
+            int x_Pos = rec.Width - this.Width;
+            
+            //int x_Shift = this.Width + 5;     // Full visible note (no overlay)
+            int x_Shift = 25;                   // Custom overlay
+            int n_columns = 0;
+            int n_max_columns = rec.Width / x_Shift;
+            bool add = false;
+            myPos = new PosXY(x_Pos, rec.Height - (this.Height * 1));
+            
+            while (!add)
+            {
+                for (int n_not = 1; n_not <= maxNot; n_not++)
+                {
+                    myPos = new PosXY(x_Pos, rec.Height - (this.Height * n_not));
+
+                    if (!posXYContains(myPos) )
+                    {
+                        add = true; break;
+                    }
+
+                    // X shift if no more column space
+                    if (n_not == maxNot)
+                    {
+                        n_columns++;
+                        n_not = 0;
+                        x_Pos = rec.Width - this.Width - x_Shift * n_columns;
+                    }
+
+                    // Last exit condition: the screen is full of note
+                    if (n_columns >= n_max_columns)
+                    {
+                        add = true; break;
+                    }
+                }
+            }
+
+            // Notify position
+            this.Location = new Point(myPos.X, myPos.Y);
+            posXY.Add(myPos);
+        }
+        #endregion
+
+        #region EVENTS
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            posXY.Remove(myPos);
+            this.Dispose();
+            this.Close();
+        }
+        #endregion
+
+        #region HELPERS
+        private bool posXYContains(PosXY myPos)
+        {
+            foreach (PosXY p in posXY)
+                if (p.X == myPos.X && p.Y == myPos.Y)
+                    return true;
+            return false;
+        }
+        #endregion
+    }
+}
