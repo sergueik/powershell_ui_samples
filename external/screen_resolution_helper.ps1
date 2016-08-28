80b96130733d35e8147ab18b1b9e46cd97999b66
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-08-28 11:33:06 -0400

    intermediate revision - added external links to similar code

--- external/screen_resolution_helper.ps1
+++ external/screen_resolution_helper.ps1
@@ -1,16 +1,19 @@
+# Alternative method:
+# https://github.com/timmui/ScreenResolutionChanger
+# http://dotnet.mvps.org/dotnet/faqs/?id=setscreenresolution&lang=en
+# http://www.codeproject.com/Articles/6810/Dynamic-Screen-Resolution
+
 $helper_type_namespace = ('Util_{0}' -f (([guid]::NewGuid()) -replace '-',''))
 $helper_type_name = 'ScreenResolutionHelper'
 
 Add-Type -UsingNamespace @(
-  'System.Drawing',
   'System.Windows.Forms'
 ) -MemberDefinition @'
 public void showScreenResolution(){
   System.Drawing.Rectangle workingRectangle = Screen.PrimaryScreen.WorkingArea;
   Console.Error.WriteLine("Width: {0}\nHeight: {1}\n",  workingRectangle.Width, workingRectangle.Height);
 }
-'@ -ReferencedAssemblies @( 'System.Windows.Forms.dll',
-     'System.Drawing.dll') -Namespace $helper_type_namespace -Name $helper_type_name -ErrorAction Stop
+'@ -ReferencedAssemblies @( 'System.Windows.Forms.dll' ) -Namespace $helper_type_namespace -Name $helper_type_name -ErrorAction Stop
 $helper = New-Object -TypeName ('{0}.{1}' -f $helper_type_namespace,$helper_type_name)
 
 try {
@@ -18,4 +21,3 @@ try {
 } catch [exception]{
   Write-Output ("Ignoring exception:`n{0}" -f $_.Exception.Message)
 }
-
