239167020cbef0576555173a023bdd5e06649e6b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-04 22:56:31 -0400

    intermediate revision - p.o.c. the basic extraction can be done. extremely fragile, unfinished

--- external/extract_gav.js
+++ external/extract_gav.js
@@ -3,6 +3,8 @@ var xmlDoc = new ActiveXObject('Msxml2.DOMDocument.3.0');
 var root;
 xmlDoc.async = false;
 xmlDoc.load('pom.xml');
+
+
 if (xmlDoc.parseError.errorCode != 0) {
    var myErr = xmlDoc.parseError;
    WScript.Echo('You have error ' + myErr.reason);
@@ -16,4 +18,17 @@ if (xmlDoc.parseError.errorCode != 0) {
       WScript.Echo(tag + ' = ' + root.childNodes.item(i).childNodes.item(0).text);
     }
   }
+  
+    for (var i = 0; i < nodeList.length; i++) {
+      WScript.Echo(tag + ' = ' + root.childNodes.item(i).childNodes.item(0).text);
+    }
+
+var xmlnode = root.childNodes.item(1);
+WScript.Echo(xmlnode.text + '\n');
+WScript.Echo('item 1:\n' + xmlnode.xml + '\n');
+var xmlnode = xmlDoc.documentElement.childNodes.item(2);
+WScript.Echo('item 2:\n' + xmlnode.nodeName + '\n');
+var xmlnode = xmlDoc.documentElement.childNodes.item(3);
+WScript.Echo('item 3:\n' +xmlnode.text + '\n');
+
 }
