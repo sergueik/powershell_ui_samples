d307b3e572ff8d1078c30c9e19b5e236558cd134
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-23 21:47:45 -0400

    intermediate revision - comfirmed commane line size limit

--- mshta/example2.cmd
+++ mshta/example2.cmd
@@ -6,13 +6,25 @@ REM Set DEBUG to true to print additional innformation to the console
 set DEBUG=false
 
 set "SCRIPT=mshta.exe "javascript:{"
-
+REM Copied code a number of times to debug the suspected script size limitaiton, which is rathed command line size limitation of cmd
 set "SCRIPT=%SCRIPT% var fso = new ActiveXObject('Scripting.FileSystemObject');"
 set "SCRIPT=%SCRIPT% var out = fso.GetStandardStream(1);"
 set "SCRIPT=%SCRIPT%   var tags = ['groupId','artifactId','version'];"
 set "SCRIPT=%SCRIPT%   for (var cnt in tags ) {"
 set "SCRIPT=%SCRIPT%     var tag = tags[cnt];"
-set "SCRIPT=%SCRIPT%     out.write(tag + '\n');"
+set "SCRIPT=%SCRIPT%     out.write(tag + ' 1\n');"
+set "SCRIPT=%SCRIPT% }"
+set "SCRIPT=%SCRIPT%   for (var cnt in tags ) {"
+set "SCRIPT=%SCRIPT%     var tag = tags[cnt];"
+set "SCRIPT=%SCRIPT%     out.write(tag + ' 2\n');"
+set "SCRIPT=%SCRIPT% }"
+set "SCRIPT=%SCRIPT%   for (var cnt in tags ) {"
+set "SCRIPT=%SCRIPT%     var tag = tags[cnt];"
+set "SCRIPT=%SCRIPT%     out.write(tag + ' 3\n');"
+set "SCRIPT=%SCRIPT% }"
+set "SCRIPT=%SCRIPT%   for (var cnt in tags ) {"
+set "SCRIPT=%SCRIPT%     var tag = tags[cnt];"
+set "SCRIPT=%SCRIPT%     out.write(tag + '4\n');"
 set "SCRIPT=%SCRIPT% }"
 set "SCRIPT=%SCRIPT% close();}""
 
