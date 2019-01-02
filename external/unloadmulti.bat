080360651d456c04dbcc0a50a6a5380360bddc77
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-01-02 09:12:42 -0500

    initial revision

---
+++ external/unloadmulti.bat
@@ -0,0 +1,26 @@
+@echo OFF
+setlocal enableextensions enabledelayedexpansion
+
+set POOLSIZE=%~z1
+REM ...
+REM my windos 2003 image is 32 bit.
+PATH=%PATH%;C:\CYGWIN\BIN
+FOR /L %%a in  (1 1 10) do @ (
+  echo "Doing %%a" ; 
+  set count=%%a
+  echo Created parameter !count!
+  set INPUTFILE=input!count!.txt
+  echo input file number !count! !INPUTFILE!
+  set OUTPUTFILE=result!count!.txt
+  echo Writing file number !count!  !outputfile!
+  call :PROCESS !count! !INPUTFILE! !outputfile!
+)
+goto :EOF
+:PROCESS
+set DATA=%1
+set INPUTFILE=%2
+set OUTPUTFILE=%3
+
+echo writing %DATA% from %INPUTFILE% to %OUTPUTFILE%
+goto :EOF
+
