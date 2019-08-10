6f5ca831834baa384b1d9ee12e38d6cee09136b3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-08-10 00:27:14 -0400

    intermeidate revision - added stackoverflow suggested fix

--- treeview_elem.ps1
+++ treeview_elem.ps1
@@ -1,9 +1,10 @@
-# http://www.cyberforum.ru/powershell/thread2489600.html
+﻿# http://www.cyberforum.ru/powershell/thread2489600.html
+# https://stackoverflow.com/questions/1283406/treenodecollection-find-doesnt-seem-to-work
 # see also: http://www.cyberforum.ru/powershell/thread2488751.html
 add-type -assembly System.Windows.Forms
-$form = New-Object System.Windows.Forms.Form
-$treeView = New-Object System.Windows.Forms.TreeView
-@(
+$form = new-object System.Windows.Forms.Form
+$treeView = new-object System.Windows.Forms.TreeView
+$labels = @(
   'первый',
   'второй',
   'третий',
@@ -12,14 +13,37 @@ $treeView = New-Object System.Windows.Forms.TreeView
   'шестой',
   'седьмой',
   'восьмой'
-) | foreach-object {
+)
+
+$labels_hash = @{}
+$cnt = 0
+$labels | foreach-object {
+  $cnt ++
+  $labels_hash[$_] = $cnt
+}
+<#
+$labels = @{
+  'первый' = 1;
+  'второй' = 2;
+  'третий' = 3;
+  'четвертый' = 4;
+  'пятый' = 5;
+  'шестой' = 6;
+  'седьмой' = 7;
+  'восьмой' =  8;
+}
+#>
+# Missing '=' operator after key in hash literal.
+$labels | foreach-object {
   $text = $_
-  $treeView.Nodes.Add($text) | out-null
+  $key = $labels_hash[$text]
+  $treeView.Nodes.Add($key, $text) | out-null
 }
 $form.Controls.add($treeView)
 # https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.treeview.nodes?view=netframework-4.0
-$text = 'Второй'
-$treeNodes = $TreeView.Nodes.Find($text, $true)
+$text = 'второй'
+$text_key = $labels_hash[$text]
+$treeNodes = $TreeView.Nodes.Find($text_key, $true)
 if ($treeNodes -ne $null) {
   $treeView.SelectedNode = $treeNodes[0]
   write-debug 'Found via API call'
