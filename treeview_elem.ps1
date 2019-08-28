58dc97cac0a2ea8f1af4e62b97c3d3e1ad5f5746
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-08-28 00:19:19 -0400

    intermediate revision - added load and paint handler ex

--- treeview_elem.ps1
+++ treeview_elem.ps1
@@ -2,6 +2,7 @@
 # https://stackoverflow.com/questions/1283406/treenodecollection-find-doesnt-seem-to-work
 # see also: http://www.cyberforum.ru/powershell/thread2488751.html
 add-type -assembly System.Windows.Forms
+add-type -assembly System.Drawing
 $form = new-object System.Windows.Forms.Form
 $treeView = new-object System.Windows.Forms.TreeView
 $labels = @(
@@ -42,6 +43,9 @@ $labels | foreach-object {
 
 $treeView.Nodes[3].Nodes.Add(42,"сорок второй")
 $form.Controls.add($treeView)
+
+
+
 # https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.treeview.nodes?view=netframework-4.0
 # added the next level subnode for demo
 $text = 'сорок второй'
@@ -60,6 +64,38 @@ if ($treeNodes -ne $null) {
   $treeView.SelectedNode = $treeNode
   write-debug 'Found through iteration'
 }
+[System.Windows.Forms.Panel] $panel = new-object System.Windows.Forms.Panel
+$form.Controls.add($panel)
+# https://toster.ru/q/661139?e=7999788#clarification_745428
+$load_code = {
+  [System.Drawing.Graphics] $g = [System.Drawing.Graphics]::FromHwnd($panel.Handle)
+  write-host 'Load event'
+  write-host $g.VisibleClipBounds.Size.Width
+  $arrayPoint = @(@( 140, 120, 180, 140 ), @( 70, 100, 60, 40 ))
+  # http://www.java2s.com/Code/CSharp/2D-Graphics/DrawPolygon.htm
+  $rc = New-Object System.Drawing.Rectangle (10,20,150,180)
+  $brush = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::Purple)
+  $g.FillRectangle($brush,$rc)
+}
+
+function paint_code {
+  param(
+    [Object]$sender,
+    [EventArgs]$e
+    )
+  [System.Drawing.Graphics] $g = [System.Drawing.Graphics]::FromHwnd($panel.Handle)
+  write-host "Paint code"
+  write-host $g.VisibleClipBounds.Size.Width
+  $arrayPoint = @(@( 140, 120, 180, 140 ), @( 70, 100, 60, 40 ))
+  # http://www.java2s.com/Code/CSharp/2D-Graphics/DrawPolygon.htm
+  $rc = New-Object System.Drawing.Rectangle (10,50,150,150)
+  $brush = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::Green)
+  $g.FillRectangle($brush,$rc)
+}
+      
+$form.add_Load($load_code)
+$form.add_Paint({ paint_code })
+# https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.panel?view=netframework-4.5
 
 $treeView.Focus() | out-null
 $form.ShowDialog() | out-null
