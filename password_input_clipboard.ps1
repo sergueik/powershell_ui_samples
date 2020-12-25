8d2f5f241eb304966d1688788fac79f34b55998d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-12-25 05:45:40 +0100

    intermediate revision - made text input and ok button focused and default, win32 way, split apart code using and plain, and clipboard and autoposition

--- password_input_clipboard.ps1
+++ password_input_clipboard.ps1
@@ -20,6 +20,8 @@
 
 param (
   [string]$user = 'demouser',
+  [switch]$clipboard,
+  [switch]$allow_automatic, # probably not useful, keep as an option to demonstrate
   [switch]$debug
 )
 
@@ -27,12 +29,18 @@ function measure_width{
   # NOTE no type declarations
   param(
     $control,
-    [System.Drawing.Font]$font
+    [System.Drawing.Font]$font,
+    # both options are not precise
+    [switch]$allow_automatic
   )
  $text_width = ($control.CreateGraphics().MeasureString($control.Text, $font).Width)
  if ($text_width -lt $control.Size.Width) {
-  # write-host ('Width: automatic {0}' -f $control.Size.Width)
-  $result = $text_width
+  write-host ('Width: automatic {0}' -f $control.Size.Width)
+  if ([bool]$PSBoundParameters['allow_automatic'].IsPresent) {
+    $result = $control.Size.Width
+  } else {
+    $result = $text_width
+  }
 } else {
   $result = $text_width
   write-host ('Width: calculated {0}' -f $result)
@@ -40,15 +48,25 @@ function measure_width{
  return $result
 }
 
-$RESULT_OK = 0
+  $RESULT_OK = 0
 $RESULT_CANCEL = 2
+
 
 function PromptPassword {
   param(
     [string]$title,
     [string]$user,
-    [object]$caller
+    [object]$caller,
+    [switch]$clipboard,
+    [switch]$allow_automatic
   )
+  $clipboard_flag = [bool]$PSBoundParameters['clipboard'].IsPresent
+
+  if ([bool]$PSBoundParameters['allow_automatic'].IsPresent) {
+  $allow_automatic_flag = '-allow_automatic'
+} else {
+  $allow_automatic_flag = $null
+}
 
   @( 'System.Drawing','System.Windows.Forms') | ForEach-Object { [void][System.Reflection.Assembly]::LoadWithPartialName($_) }
 
@@ -105,10 +123,34 @@ function PromptPassword {
   $bCancel.Text = 'Cancel'
   $bCancel.Name = 'btnCancel'
   $bCancel.AutoSize = $true
-  $w = measure_width  -font $f.Font -control $bCancel
+  $w = measure_width  -font $f.Font -control $bCancel $allow_automatic_flag
   write-host ('measure_width: {0}' -f $w)
   $bCancel.Location = new-object System.Drawing.Point(($f.Size.Width - $w - $right_margin), $bOK.Location.y)
   $f.Controls.Add($bCancel)
+<#
+
+  $bE1 = new-object System.Windows.Forms.Button
+  $bE1.Text = 'Really Cancel'
+  $bE1.Name = 'bE1'
+  $bE1.AutoSize = $true
+  $right_margin = 60
+  $w = measure_width  -font $f.Font -control $bE1 $allow_automatic_flag
+  write-host ('measure_width: {0}' -f $w)
+
+  $bE1.Location = new-object System.Drawing.Point(($f.Size.Width - $w - $right_margin), ($bCancel.Location.y + $bCancel.Size.Height + $margin_y))
+  $f.Controls.Add($bE1)
+
+  $bE2 = new-object System.Windows.Forms.Button
+  $bE2.Text = 'Really Really Cancel'
+  $bE2.Name = 'bE2'
+  $bE2.AutoSize = $true
+  $right_margin = 60
+  $w = measure_width  -font $f.Font -control $bE2 $allow_automatic_flag
+  write-host ('measure_width: {0}' -f $w)
+
+  $bE2.Location = new-object System.Drawing.Point(($f.Size.Width - $w - $right_margin), ($bE1.Location.y + $bE1.Size.Height + $margin_y))
+  $f.Controls.Add($bE2)
+#>
 <#
   $f.SuspendLayout()
   $f.Controls.AddRange(@(
@@ -124,21 +166,50 @@ function PromptPassword {
 #>
 
   $bCancel.add_click({
-    $caller.txtPassword = $null
-    $caller.txtUser = $null
+    if ($clipboard_flag) {
+      [System.Windows.Forms.Clipboard]::Clear()
+    } else {
+      $caller.txtPassword = $null
+      $caller.txtUser = $null
+    }
     $f.Close()
   })
   $bOK.add_click({
-    $caller.Data = $RESULT_OK
-    $caller.txtPassword = $t2.Text
-    $caller.txtUser = $t1.Text
+    if ($clipboard_flag) {
+      $Password = $t2.Text
+      $user = $t1.Text
+
+      [String]$cred = "${user}:${Password}"
+      write-host ('Encoding {0}' -f $cred)
+      $encodedCred = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($cred))
+
+      # $basicAuthValue = "Basic $encodedCred"
+      #
+      # $Headers = @{
+      #  Authorization = $basicAuthValue
+      # }
+
+      # ContentType = 'application/json'
+      # $uri = 'https://www.github.com
+      # Invoke-WebRequest -Uri $uri -ContentType 'text/json' -Headers $Headers
+
+      [System.Windows.Forms.Clipboard]::SetText($encodedCred)
+    } else {
+      $caller.Data = $RESULT_OK
+      $caller.txtPassword = $t2.Text
+      $caller.txtUser = $t1.Text
+    }
     $f.Close()
   })
 
   $f.Controls.Add($l)
   $f.Topmost = $true
 
-  $caller.Data = $RESULT_CANCEL
+  if ($clipboard_flag) {
+      [System.Windows.Forms.Clipboard]::Clear()
+  } else {
+    $caller.Data = $RESULT_CANCEL
+  }
   $f.Add_Shown({
     $f.ActiveControl = $t2
     $f.Activate()
@@ -146,13 +217,21 @@ function PromptPassword {
   $f.KeyPreview = $True
   $f.Add_KeyDown({
     if ($_.KeyCode -eq 'Escape') {
-      $caller.Data = $RESULT_CANCEL
+      if ($clipboard_flag) {
+        [System.Windows.Forms.Clipboard]::Clear()
+      } else {
+        $caller.Data = $RESULT_CANCEL
+      }
     }
     else { return }
     $f.Close()
   })
 
-  [void]$f.ShowDialog([win32window]($caller))
+  if ($clipboard_flag) {
+    [void]$f.ShowDialog()
+  } else {
+    [void]$f.ShowDialog([win32window]($caller))
+  }
   $f.Dispose()
 }
 
@@ -200,6 +279,12 @@ public class Win32Window : IWin32Window
 [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
 [void][System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')
 
+$clipboard_flag = [bool]$PSBoundParameters['clipboard'].IsPresent
+if ([bool]$PSBoundParameters['allow_automatic'].IsPresent) {
+  $allow_automatic_flag = '-allow_automatic'
+} else {
+  $allow_automatic_flag = $null
+}
 $debug_flag = [bool]$PSBoundParameters['debug'].IsPresent
 if ($debug_flag){
   $DebugPreference = 'Continue'
@@ -219,21 +304,35 @@ Copyright (C) 2015 Microsoft Corporation. All rights reserved.
 [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle
 0
 #>
+$o = new-object Helper
+if (-not $clipboard_flag) {
+  write-output '1'
+  $window_handle = [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle
+  write-output ('Using current process handle {0}' -f $window_handle)
+  if ($window_handle -eq 0) {
+    $processid = [System.Diagnostics.Process]::GetCurrentProcess().Id
+    $parent_process_id = get-wmiobject win32_process | where-object {$_.processid -eq  $processid } | select-object -expandproperty parentprocessid
 
-$window_handle = [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle
-write-output ('Using current process handle {0}' -f $window_handle)
-if ($window_handle -eq 0) {
-  $processid = [System.Diagnostics.Process]::GetCurrentProcess().Id
-  $parent_process_id = get-wmiobject win32_process | where-object {$_.processid -eq  $processid } | select-object -expandproperty parentprocessid
+    $window_handle = get-process -id $parent_process_id | select-object -expandproperty MainWindowHandle $allow_automatic_flag
+    write-output ('Using current process parent process {0} handle {1}' -f $parent_process_id, $window_handle)
+  }
 
-  $window_handle = get-process -id $parent_process_id | select-object -expandproperty MainWindowHandle
-  write-output ('Using current process parent process {0} handle {1}' -f $parent_process_id, $window_handle)
-}
+  $caller = new-object Win32Window -ArgumentList ($window_handle)
+  PromptPassword -Title $title -user $user -caller $caller
+  if ($caller.Data -ne $RESULT_CANCEL) {
+    if ($debug_flag){
+      Write-Debug ('Original username/password was: {0} / {1}' -f $caller.txtUser, $caller.txtPassword)
+    }    
+  }
+} else {
+  PromptPassword -Title $title -user $user -clipboard $allow_automatic_flag
+  $data = [System.Windows.Forms.Clipboard]::GetText()
+  if ($debug){
+  [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String( $data))
 
-$caller = new-object Win32Window -ArgumentList ($window_handle)
-PromptPassword -Title $title -user $user -caller $caller
-if ($caller.Data -ne $RESULT_CANCEL) {
-  if ($debug_flag){
-    Write-Debug ('Original username/password was: {0} / {1}' -f $caller.txtUser, $caller.txtPassword)
-  }    
+  } else {
+    write-output ('clipboard: {0}' -f $data)
+  }
+  [System.Windows.Forms.Clipboard]::Clear()
+
 }
