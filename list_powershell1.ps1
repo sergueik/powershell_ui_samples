8da028b28e36e956b24bcd10a6e2d3b63ad8e92d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-02-12 22:40:42 -0500

    intermediate revision - minor ui fix of presetting selection

--- list_powershell1.ps1
+++ list_powershell1.ps1
@@ -1,3 +1,22 @@
+#Copyright (c) 2014,2021 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
 Add-Type -TypeDefinition @"
 
 // "
@@ -72,7 +91,8 @@ $objListBox.Height = 80
 [void]$objListBox.Items.Add("node1")
 [void]$objListBox.Items.Add("node2")
 [void]$objListBox.Items.Add("node3")
-
+# https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.listbox.setselected
+$objListBox.setSelected(1, $true)
 $objForm.Controls.Add($objListBox)
 
 $objForm.Topmost = $True
