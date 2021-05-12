b910bdd48ea0eaeae5f82f2d2fb7fc8b2e2697dc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-05-12 02:21:15 -0400

    intermediate revision - provided better arguments

--- utils/place_user_startup.ps1
+++ utils/place_user_startup.ps1
@@ -1,7 +1,21 @@
 param (
-  [String] $script_path = 'dummy.cmd'
+  [String] $script_dir = (get-location),
+  [String] $script_name = 'dummy.cmd',
+  [String] $user_name = $null,
+  [switch] $remove
 )
-
-if (test-path -path $script_path ) {
-  copy-item -destination "${env:USERPROFILE}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -force -path $script_path
+if (($user_name -ne $null) -and ([Security.Principal.WindowsIdentity]::GetCurrent().Name  -ne $user_name) ) {
+  write-output ('This script is supposed to run by user {0}' -f $user_name)
+  return
+}
+$script_path = "${script_dir}\${script_name}"
+if ($remove){
+  $full_path = "${env:USERPROFILE}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\${script_name}"
+  if (test-path -path $full_path ) {
+    remove-item -literalpath $full_path -force
+  }
+} else {
+  if (test-path -path $script_path ) {
+    copy-item -destination "${env:USERPROFILE}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -force -path $script_path
+  }
 }
